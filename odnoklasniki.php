c7bb16c3f081cd029e037727716ddc487ad9a77b
exytab <exytab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-16 19:21:23 +0000

    new feature: right and left
    git-svn-id: https://plugins.svn.wordpress.org/odnoklasniki-share-button/trunk@312198 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- odnoklasniki.php
+++ odnoklasniki.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Odnoklasniki Share Button
-Version: 1.0
+Version: 1.1
 Plugin URI: http://exy.com.ua/
 Description: Add Odnoklasniki Share Button
 Author: ExyTab
@@ -25,19 +25,19 @@ Author URI: http://exy.com.ua/
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-
+		add_option('show', '1');
 		add_option('in_general', '0');
 		add_option('position', '1');
-		add_option('size', '1');
-		add_option('text', 'Класс!');
+		add_option('side', 'right');
+		add_option('size', 'odkl-klass');
+
+
 
 		
 add_action('wp_head', 'odnoklasniki_head');
 
 function odnoklasniki_head() {
-
 echo '<link rel="stylesheet" href="http://stg.odnoklassniki.ru/share/odkl_share.css"><script src="http://stg.odnoklassniki.ru/share/odkl_share.js" type="text/javascript"></script>';
-
 }
 		
 		
@@ -46,27 +46,47 @@ add_action('admin_menu', 'odno_add_tools_menu');
 
 
 function odno_add_tools_menu() {
-
 add_options_page('Odnoklasniki Share Button', 'Odnoklasniki Share Button', 'manage_options', '', 'tp_manage_menu');
-
 }
 
+//setting
 
 function tp_manage_menu(){
 
- 
+ echo '<div class="wrap">';
+ echo '<h2>Одноклассники. Share Button. Настройка</h2>';
+
      if($_REQUEST['do'] == 'save')
      {
+          update_option('show', $_REQUEST['show']);
           update_option('in_general', $_REQUEST['in_general']);
           update_option('position', $_REQUEST['position']);
           update_option('size', $_REQUEST['size']);
-          update_option('text', $_REQUEST['text']);
+          update_option('side', $_REQUEST['side']);
           echo '<div id="message" class="updated"><p><strong>Параметры изменились</strong></p></div>';
      }
 
 
-echo '<form action="" method="post">
+echo '<table><tr><td><form action="" method="post">
     <table class="form-table" border="0">
+     <tr>
+
+          <td width="360">Показывать кнопку?</td>
+          <td><input type="radio" name="show" value="1" ';
+
+if (get_option('show', '')==1) echo 'checked';
+
+echo '> Да <br>
+		<input type="radio" name="show" value="0" ';
+if (get_option('show', '')==0) echo 'checked';
+echo '> Нет  
+	</td>
+     </tr>
+     <tr>
+
+          <td colspan="2">(При "Нет" только загружаются библиотеки)</td>
+     </tr>
+
      <tr>
 
           <td width="360">Выводить на главную страницу?</td>
@@ -82,7 +102,7 @@ echo '> Нет
      </tr>
 
 	<tr>
-          <td width="360">Положение</td>
+          <td width="360">Положение:</td>
           <td><input type="radio" name="position" value="1" ';
 
 if (get_option('position', '')==1) echo 'checked';
@@ -90,85 +110,84 @@ if (get_option('position', '')==1) echo 'checked';
 echo '> Сверху <br>
 		<input type="radio" name="position" value="0" ';
 if (get_option('position', '')==0) echo 'checked';
-echo '> Снизу  
+echo '> Снизу  <bh>
+<hr>
+<input type="radio" name="side" value="right" ';
+
+if (get_option('side', '')=='right') echo 'checked';
+
+echo '> Справа <br>
+		<input type="radio" name="side" value="left" ';
+if (get_option('side', '')=='left') echo 'checked';
+echo '> Слева  
 	</td>
      </tr>
 
 	<tr>
           <td width="360">Размер</td>
-          <td><input type="radio" name="size" value="1" ';
+          <td><input type="radio" name="size" value="odkl-klass" ';
 
-if (get_option('size', '')==1) echo 'checked';
+if (get_option('size', '')=='odkl-klass') echo 'checked';
 
-echo '> Большой <br>
-		<input type="radio" name="size" value="0" ';
-if (get_option('size', '')==0) echo 'checked';
-echo '> Маленький  
+echo '> <img border="0" src="/wp-content/plugins/odnoklasniki/big.png"> <br>
+		<input type="radio" name="size" value="odkl-klass-s" ';
+if (get_option('size', '')=='odkl-klass-s') echo 'checked';
+echo '> <img border="0" src="/wp-content/plugins/odnoklasniki/small.png"> 
 	</td>
      </tr>
 
-	<tr>
-          <td width="360">Текст кнопки</td>
-          <td>
-		<input type="text"  name="text"  value="' . get_option('text', '') . '">
-	</td>
-     </tr> 
 	</table>
 	<input type="hidden" name="do" value="save">
 	<br>
 	<input type="submit" name="submit" value="Сохранить" class="button-primary">
      </form>
+</td><td align="right" valign="top" width="200">
+<table cellpadding="0" cellspacing="0" border="0" style="font: 0.8em Arial, sans-serif"><tr><td width="116" height="77" style="border: 0; background:url(https://img.yandex.net/i/money/top-5rub-default.gif) repeat-y; text-align:center; padding: 0;" align="center" valign="bottom"><form style="margin: 0; padding: 0 0 2px;" action="https://money.yandex.ru/donate.xml" method="post"><input type="hidden" name="to" value="41001677446224"/><input type="hidden" name="s5" value="5rub"/><input type="submit" value="Дай пять"/></form></td></tr><tr><td width="116" height="38" style="font-size:13px; color:black;padding: 0; border: 0; background:url(https://img.yandex.net/i/money/bg-default.gif) repeat-y; text-align:center; padding: 5px 0;" align="center" valign="top"><b>За плагин</b></td></tr><tr><td style="padding: 0; border:0;"><img src="https://img.yandex.net/i/money/bottom-default.gif" width="116" height="40" alt="" usemap="#button" border="0" /><map name="button"><area alt="Яндекс" coords="38,2,49,21" href="http://www.yandex.ru"><area alt="Яндекс. Деньги" coords="52,1,84,28" href="https://money.yandex.ru"><area alt="Хочу такую же кнопку" coords="17,29,100,40" href="https://money.yandex.ru/choose-banner.xml"></map></td></tr></table>
+</td></tr></table>';
 
-';
+
+  echo '</div>';
+
 }
 //end setting
 
 function add_post_odnoklasniki_content($content) {
+if (get_option('show', '')==1){
+
+
 	if (get_option('in_general', '')==1)	{
 		if(!is_feed() ) {
 			if(get_option('position', '')==1){
-				if(get_option('size', '')==1){	
-$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass href=" . get_permalink() . ">' . get_option('text', '') . '</a></div>" . $content;
-				}
-				if(get_option('size', '')==0){	
-$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass-s href=" . get_permalink() . "></a></div>" . $content;
-				}
+
+$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" style=\"float: " . get_option('side', '') . "; margin: 0px 0px 0px 5px;\" class=" . get_option('size', '') . " href=" . get_permalink() . "></a></div>" . $content;
+
 			}
 			if(get_option('position', '')==0){
-				if(get_option('size', '')==1){	
-$content= $content .  "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass href=" . get_permalink() . ">' . get_option('text', '') . '</a></div>" ;
-				}
-				if(get_option('size', '')==0){	
-$content= $content . "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass-s href=" . get_permalink() . "></a></div>" ;
-				}
+
+$content= $content . "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" style=\"float: " . get_option('side', '') . "; margin: 0px 0px 0px 5px;\" class=" . get_option('size', '') . " href=" . get_permalink() . "></a></div>" ;
+
 			}
 		}
 	}
 	if (get_option('in_general', '')==0)	{
 		if(!is_feed() && !is_home() ) {
 			if(get_option('position', '')==1){
-				if(get_option('size', '')==1){	
-$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass href=" . get_permalink() . ">' . get_option('text', '') . '</a></div>" . $content;
-				}
-				if(get_option('size', '')==0){	
-$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass-s href=" . get_permalink() . "></a></div>" . $content;
-				}
+$content= "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" style=\"float: " . get_option('side', '') . "; margin: 0px 0px 0px 5px;\" class=" . get_option('size', '') . " href=" . get_permalink() . "></a></div>" . $content;
+
 			}
 			if(get_option('position', '')==0){
-				if(get_option('size', '')==1){	
-$content= $content .  "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass href=" . get_permalink() . ">' . get_option('text', '') . '</a></div>" ;
-				}
-				if(get_option('size', '')==0){	
-$content= $content . "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" class=odkl-klass-s href=" . get_permalink() . "></a></div>" ;
-				}
+$content = $content . "<div id=\"odno\"><a onclick=\"ODKL.Share(this);return false;\" style=\"float: " . get_option('side', '') . "; margin: 0px 0px 0px 5px;\" class=" . get_option('size', '') . " href=" . get_permalink() . "></a></div>" ;
+
 			}
 		}
 	}
-	
 
 
 
-  
+
+ }
+
+
   
   return $content;
 }
--- readme.txt
+++ readme.txt
@@ -1,35 +1,85 @@
 === Odnoklasniki Share Button ===
-Contributors: exytab	
+
+Contributors: exytab   
+
 Donate link: http://exy.com.ua/
+
 Tags: Odnoklasniki, Share, Button
+
 Requires at least: 3.0
+
 Tested up to: 3.0.1
+
 Stable tag: /function/
+
+ 
 
 Add Odnoklasniki Share Button
 
+ 
+
 == Description ==
+
 Плагин позволяет добавлять Share Button от соц. сети Одноклассники.
+
 Есть несколько настроек:
-=Выводить или нет на гласную страницу.
-=Положение иконки
-=Размер иконки
-=Текст подписи
+
+Выводить или нет на главную страницу.
+
+Положение иконки
+
+Размер иконки
+
 == Installation ==
-1. Upload odnoklasniki.zip to the /wp-content/plugins/ directory
-2. Unzip it
-3. Activate the plugin through the Plugins menu in WordPress
-4. Use the Settings->Odnoklasniki Share Button page to change your plugin options.
+
+1. Скачать odnoklasniki.zip в /wp-content/plugins/
+
+2. Распаковать
+
+3. Активировать Плагин через меню в WordPress
+
+4. Используйте Настройки->Odnoklasniki Share Button что бы изменить вид кнопки
+
 == Frequently Asked Questions ==
+
 How can I request that you improve or add a feature?
-Visit <a href=http://exy.com.ua/index.php/odnoklassniki-share-button-plagin-pod-wordpress/ > plugin page </a>on my site and use contact form or post a comment.
+
+Visit <a href="http://exy.com.ua/index.php/odnoklasniki-share-button-1-1/" > plugin page </a>on my site and use contact form or post a comment.
+
 == Screenshots ==
+
 1. Settings page
+
+<img src=”http://exy.com.ua/plugin/odnoklasniki/menu.png”>
+
 2. Button types examples
+
+<img src=”http://exy.com.ua/plugin/odnoklasniki/buttons.png”>
+
+ 
+
 == Changelog ==
+
 1.0
+
 First public release
-= 1.0 =
-* Release version.
+
+1.1
+
+Добавлена возможность переноса кнопки направо.
+
+Исправлены мелкие баги
+
+= 1.1 =
+
+* Second version.
+
 == Upgrade Notice ==
-First public release
+
+Добавлена возможность переноса кнопки направо.
+
+Исправлены мелкие баги
+
+ 
+
+ 
